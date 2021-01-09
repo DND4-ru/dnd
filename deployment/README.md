389cb7edc96f807e4722f7162315be46561e3975
Artem Kuskis <artem.kuskis@gmail.com>
2021-01-09 19:13:30 +0300

    Add mongo express

--- deployment/README.md
+++ deployment/README.md
@@ -1,3 +1,3 @@
 ### Change default secrets
 
-Script `change_secrets.sh` will help you to change secrets in interactive way.
+Use `change_secrets.sh` to change secrets in interactive way.
--- deployment/change_secrets.sh
+++ deployment/change_secrets.sh
@@ -2,13 +2,25 @@
 
 set -e
 
-function change_mongodb_secrets {
+function change_mongo_secrets {
   echo "Setup mongodb secrets"
   read -p "* User name: " -r user_name
   read -sp "* Password: " -r password
+  echo ""
 
-  sed -i "s/^\( *mongo-root-username:\).*$/\1 $(echo -n ${user_name} | base64)/" mongodb-secret.yaml
-  sed -i "s/^\( *mongo-root-password:\).*$/\1 $(echo -n ${password} | base64)/" mongodb-secret.yaml
+  sed -i "s/^\( *mongo-root-username:\).*$/\1 $(echo -n ${user_name} | base64)/" mongo-secret.yaml
+  sed -i "s/^\( *mongo-root-password:\).*$/\1 $(echo -n ${password} | base64)/" mongo-secret.yaml
 }
 
-change_mongodb_secrets
+function change_mongo_express_secrets {
+  echo "Setup mongodb-express secrets"
+  read -p "* User name: " -r user_name
+  read -sp "* Password: " -r password
+  echo ""
+
+  sed -i "s/^\( *mongo-express-username:\).*$/\1 $(echo -n ${user_name} | base64)/" mongo-express-secret.yaml
+  sed -i "s/^\( *mongo-express-password:\).*$/\1 $(echo -n ${password} | base64)/" mongo-express-secret.yaml
+}
+
+change_mongo_secrets
+change_mongo_express_secrets
